fe50bb8ed2bfa9c0d65676156ef1429ed935547b
wang chao <cwang@thoughtworks.com>
2013-01-20 21:36:07 +0800

    day2 arrays

--- ruby/day2.rb
+++ ruby/day2.rb
@@ -10,3 +10,17 @@ f=File.open('readfile')
 f.each do |line|
 	puts line
 end
+
+#print every 4 numbers in array
+array = (1..16).to_a
+array.each do |number|
+	if number%4 == 0
+		print "#{number}\n"
+	else
+		print "#{number}  "
+	end
+end
+
+#print using each_slice
+array = Array.new(16) { |x| x }
+array.each_slice(4) {|x| p x}
